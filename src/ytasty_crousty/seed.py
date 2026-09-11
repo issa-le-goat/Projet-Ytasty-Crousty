98b233a60873619abe2cf47f4eb037ef75de39e1
Garchilla <dylan.king@ynov.com>
2026-09-11 17:10:04 +0200

    feat: rajout des imports de seed

--- src/ytasty_crousty/seed.py
+++ src/ytasty_crousty/seed.py
@@ -2,3 +2,6 @@ from ytasty_crousty.database import SessionLocal, Base, engine
 
 from modules.restaurants.models import Restaurant
 from modules.users.models import User, RoleEnum
+from modules.products.models import Product
+from modules.ordres.models import Order, OrderItem
+from modules.auths.security import pasword_hash
