ff2ac63a620ff6453acbd6a2c3a70009b9e0d3b3
Garchilla <dylan.king@ynov.com>
2026-09-11 17:18:13 +0200

    feat: debut de seed

--- src/ytasty_crousty/seed.py
+++ src/ytasty_crousty/seed.py
@@ -5,3 +5,13 @@ from modules.users.models import User, RoleEnum
 from modules.products.models import Product
 from modules.ordres.models import Order, OrderItem
 from modules.auths.security import pasword_hash
+
+RESTAURANT_NAMES = [
+    ("Ytasty Crousty Aix", "Aix-en-Provence"),
+    ("Ytasty Crousty Paris", "Paris"),
+    ("Ytasty Crousty Lyon", "Lyon"),
+]
+
+def seed():
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
